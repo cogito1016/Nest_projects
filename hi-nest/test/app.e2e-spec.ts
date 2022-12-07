0696442294bf16b7b301654b5d71f06912731abf
김재형 <cogito1016@naver.com>
2022-12-07 15:03:50 +0900

    test: e2e +

--- hi-nest/test/app.e2e-spec.ts
+++ hi-nest/test/app.e2e-spec.ts
@@ -30,21 +30,27 @@ describe('AppController (e2e)', () => {
   });
 
   describe("/movies", ()=>{
-    it("GET",()=> {
+    it("GET 200",()=> {
       return request(app.getHttpServer())
       .get("/movies")
       .expect(200)
       .expect([]);
     })
 
-    it("POST",()=>{
+    it("POST 201",()=>{
       return request(app.getHttpServer())
       .post('/movies')
       .send({title:"TestMovie",genres:['공포','로맨스'],year:2020})
       .expect(201);
     })
 
-    it("DELETE",()=>{
+    it('POST 400',()=>{
+      return request(app.getHttpServer())
+      .post("/movies")
+      .expect(400)
+    })
+
+    it("DELETE 404",()=>{
       return request(app.getHttpServer())
       .delete('/movies')
       .expect(404);
@@ -52,18 +58,29 @@ describe('AppController (e2e)', () => {
   })
 
   describe('/movies/:id',()=>{
-    // it.todo("GET");
     it("GET 200", ()=>{
       return request(app.getHttpServer())
       .get("/movies/1")
       .expect(200);
     });
+
     it('GET 404',()=>{
       return request(app.getHttpServer())
       .get("/movies/9999")
       .expect(404);
     });
-    // it.todo("DELETE");
-    // it.todo("PATCH");
+
+    it('PATCH 200',()=>{
+      return request(app.getHttpServer())
+      .patch("/movies/1")
+      .send({title:"Updated Title"})
+      .expect(200)
+    })
+
+    it('DELETE 200',()=>{
+      return request(app.getHttpServer())
+      .delete("/movies/1")
+      .expect(200);
+    })
   })
 });
