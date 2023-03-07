06a4772608886d9a49fa41d29ed67d9cccf1b301
김재형 <cogito1016@naver.com>
2023-03-07 18:03:02 +0900

    feat: devtools

--- test_01/src/app.module.ts
+++ test_01/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    DevtoolsModule.register({
+      http: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
