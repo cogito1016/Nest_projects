4cdf681ed0e14373f03150556770fc4cd5dd1e5c
김재형 <cogito1016@naver.com>
2023-05-26 01:29:24 +0900

    feat: auth, 로그인 간단버전

--- auth-server/auth/auth.service.ts
+++ auth-server/auth/auth.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(inputUsername: string, inputPassword: string): Promise<any> {
+    const user = await this.usersService.findOne(inputUsername);
+    if (user?.password !== inputPassword) {
+      throw new UnauthorizedException();
+    }
+
+    const { password, ...result } = user;
+    //TODO: JWT토큰을 생성하고 반환하는걸로 전환해야 함
+    return result;
+  }
+}
