465adf1581d279cd0305e253adad03041ce0dd9f
김재형 <cogito1016@naver.com>
2023-05-26 01:26:05 +0900

    feat: User Service 목데이터를 활용한 findOne

--- auth-server/users/users.service.ts
+++ auth-server/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    //TODO: MVP를 위한 목데이터
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'john',
+      password: 'changeme',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
