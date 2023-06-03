bfd279072ad119dd27856f3905970853376b6953
김재형 <cogito1016@naver.com>
2023-06-03 21:23:43 +0900

    feat: express 기본

--- express_01/src/app.ts
+++ express_01/src/app.ts
@@ -1,1 +1,14 @@
-console.log('hi');
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as express from 'express';
+
+const app: express.Express = express();
+
+const port = 8000;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log('This is the example');
+});
