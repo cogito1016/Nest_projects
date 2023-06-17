ab2aed5920a68d02432f9b2a411e6b0ca22bdadb
김재형 <cogito1016@naver.com>
2023-06-17 16:38:28 +0900

    decorator기능학습 : 학습을위한 구조변경

--- test_01/src/app.controller.ts
+++ test_01/src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.decoTest();
+    return this.appService.decoComposition();
   }
 }
--- test_01/src/app.service.ts
+++ test_01/src/app.service.ts
@@ -1,41 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DecoEvaluationTest } from './deco_evaluation_test';
 
 @Injectable()
 export class AppService {
   decoTest(): string {
-    const testInstance = new TestClass();
+    const testInstance = new DecoEvaluationTest();
     testInstance.decorateTest1();
     return 'Hello World!';
   }
-}
 
-function deco_1(
-  target: any,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-) {
-  console.log('데코레이터 deco_1 가 평가합니다');
-}
-
-function deco_2(value: string) {
-  console.log('데코레이터 deco_2 가 평가합니다');
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    console.log(value);
-  };
-}
-
-class TestClass {
-  @deco_1
-  decorateTest1() {
-    console.log('함수 decorateTest1 가 호출됩니다.');
-  }
-
-  @deco_2('Hello')
-  decorateTest2() {
-    console.log('함수 decorateTest2 가 호출됩니다.');
+  decoComposition(): string {
+    return 'composition test';
   }
 }
