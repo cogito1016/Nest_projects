b29e788aac178c096fe2114906eb049fe0ef6031
김재형 <cogito1016@naver.com>
2023-06-17 16:35:12 +0900

    decorator기능학습(1) : 왜 호출할때마다 평가를안하지?

--- express_01/src/app.ts
+++ express_01/src/app.ts
@@ -8,7 +8,3 @@ const port = 8000;
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World');
 });
-
-app.listen(port, () => {
-  console.log('This is the example');
-});
--- test_01/src/app.service.ts
+++ test_01/src/app.service.ts
@@ -2,7 +2,40 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  decoTest(): string {
+    const testInstance = new TestClass();
+    testInstance.decorateTest1();
     return 'Hello World!';
+  }
+}
+
+function deco_1(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  console.log('데코레이터 deco_1 가 평가합니다');
+}
+
+function deco_2(value: string) {
+  console.log('데코레이터 deco_2 가 평가합니다');
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log(value);
+  };
+}
+
+class TestClass {
+  @deco_1
+  decorateTest1() {
+    console.log('함수 decorateTest1 가 호출됩니다.');
+  }
+
+  @deco_2('Hello')
+  decorateTest2() {
+    console.log('함수 decorateTest2 가 호출됩니다.');
   }
 }
